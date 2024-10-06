08ee338211b74c8d34abc6ad90803467b2513571
Kyle Sawicki <kcool3000@gmail.com>
2024-10-06 04:24:49 -0400

    changed landing page

--- kanbas-react-web-app/src/App.tsx
+++ kanbas-react-web-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="Kanbas" />} />
+        <Route path="/" element={<Navigate to="Labs" />} />
         <Route path="/Labs/*" element={<Labs />} />
         <Route path="/Kanbas/*" element={<Kanbas />} />
       </Routes>
